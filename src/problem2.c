34a8ec5680d879f47349e06bdc13d59e22ea5f36
Kieran Bingham <kieranbingham@gmail.com>
2012-02-17 12:54:54 +0000

    Problems: Problem 2 : Fix an 'out by 2' error, and refactor

--- src/problem2.c
+++ src/problem2.c
@@ -1,11 +1,23 @@
 #include "common.h"
 
+inline unsigned int FibonacciNext(unsigned int *first, unsigned int *second)
+{
+  unsigned int next = *first + *second;
+
+  /* Move the number window */
+  *first = *second;
+  *second = next;
+
+  return next;
+}
+
+
 int problem2(int argc, char ** argv)
 {
 #define FOURMILLION 4000000
 
   unsigned int first = 1;
-  unsigned int second = 2;
+  unsigned int second = 1;
   unsigned long long sum = 0;
 
   int target = FOURMILLION;
@@ -13,18 +25,11 @@ int problem2(int argc, char ** argv)
   if (argc > 1)
     target = atoi(argv[1]);
 
-  while (first < target && second < target)
+  while ( FibonacciNext(&first, &second) <= target )
   {
-    first += second;
-
-    if ((first & 0x1) == 0) 
-       sum += first;
-
-    second += first;
-    
+    /* The 'second' value is now our next value */
     if ((second & 0x1) == 0)
        sum += second;
-
   }
 
   printf("Sum = %lld\n", sum);
